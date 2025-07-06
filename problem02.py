e09a57903f64af99bc2faf8519ba551645469fa0
Kewvy <Kew8016@gmail.com>
2025-07-06 20:10:34 +0700

    problem 02

--- problem02.py
+++ problem02.py
@@ -1,12 +1,12 @@
-def find_multiples_three(start,end):
+def find_multiples_three_and_four(start,end):
     num_list = []
     if start < 1 or end>1000:
         return print("Please enter 1 between 1000")
     elif start>=end:
         return print(f"input: start = {start}, end = {end}\noutput: {num_list}" )
     for i in range(start,end+1):
-        if i%3 == 0:
+        if i%3 == 0 and i%4==0:
             num_list.append(i)
     return print(f"input: start = {start}, end = {end}\noutput: {num_list}" )
 
-find_multiples_three(10,25)
+find_multiples_three_and_four(10,50)
